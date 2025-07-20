90011d6dd16eea62fd89dd7358a6610863028db1
Terence Lee <whoisterencelee@hotmail.com>
2025-07-20 09:31:08 +0800

    battle tested

--- index.js
+++ index.js
@@ -19,9 +19,11 @@ const url = require('url');
 
 // Container Object
 const server = {};
+server.router = {}  // handles dynamic content with a data chunks receive complete
+server.express = {} // don't even handles data chunks just pass request to middleware with some additional express properties
 
 // Base directory - Assuming minimal-dynamic-http-server will be accessed from its own folder
-const baseDir = path.join(__dirname, '../');
+const baseDir = path.join(__dirname, '../www/');  // remember the slash at the end
 
 /**
  *
@@ -31,6 +33,7 @@ const baseDir = path.join(__dirname, '../');
 
 // Allowed Mime types for static content
 const mimeTypes = {
+  '.txt' : 'text/plain',
   '.html': 'text/html',
   '.jgp': 'image/jpeg',
   '.css': 'text/css',
@@ -38,7 +41,43 @@ const mimeTypes = {
   '.png': 'image/png',
   '.ico': 'image/x-icon',
   '.json': 'application/json',
+  '.rss': 'application/rss+xml',
+  '.xml': 'application/xml',
 };
+
+const corheaders = {  // reference https://bigcodenerd.org/enable-cors-node-js-without-express/
+	'Access-Control-Allow-Origin': '*' ,
+	'Access-Control-Allow-Methods': 'OPTIONS, POST, GET, PUT, PATCH, DELETE' ,
+	'Access-Control-Allow-Headers' : 'Origin,Authorization,X-Requested-With,content-type,Accept' ,
+	'Access-Control-Max-Age': 2592000, // 30 days
+	//'Access-Control-Allow-Credentials' : true
+	/** add other headers as per requirement */
+
+	//'Set-Cookie' : 'user=chocochipchip; Secure; Path' ,  
+	// thought about using cookie to store user, but is won't work
+	// since cookie will only be store for the form submit url at functional.limited
+	// Path won't work, because the url is still fixed functional.limited/subscribe
+	// Domain is used for only subdomain cookie
+	// cookie needs expire date otherwise become session cookie
+	// seems cookie wont work for me, just use devicekey
+};
+
+// JSON.stringify for request object which has circular references
+//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Errors/Cyclic_object_value#Examples
+const getCircularReplacer = () => {
+        const seen = new WeakSet();
+        return (key, value) => {
+                if (typeof value === "object" && value !== null) {
+                        if (seen.has(value)) {
+                                return;
+                        }
+                        seen.add(value);
+                }
+                return value;
+        };
+};
+var json = ( c ) => JSON.stringify( c, getCircularReplacer() )
+server.json = json
 
 /**
  * Get the content type for a given path
@@ -70,16 +109,14 @@ server.getContentType = url => {
 server.serveStaticContent = (pathname, response) => {
   // Get content type based on the file extension passed in the request url
   const contentType = server.getContentType(pathname);
-  // Set the Content-Type response header
-  response.setHeader('Content-Type', contentType);
 
   // Read the file and send the response
   fs.readFile(`${baseDir}${pathname}`, (error, data) => {
     if (!error) {
-      response.writeHead(200);
+      response.writeHead(200 , Object.assign( { 'Content-Type' : contentType } , corheaders ) )
       response.end(data);
     } else {
-      response.writeHead(404);
+      response.writeHead(404 , corheaders );
       response.end('404 - File Not Found');
     }
   });
@@ -91,34 +128,18 @@ server.serveStaticContent = (pathname, response) => {
  */
 
 /**
- * If incoming path is one of the allowed dynamic paths then return the path
- * else return false
- * @param {string} path
- */
-server.getAllowedDynamicPath = path => {
-  for (const key in server.router) {
-    if (server.router.hasOwnProperty(key)) {
-      if (path === key) {
-        return path;
-      }
-    }
-  }
-  return false;
-};
-
-/**
  * Serve the dynamic content
  * @param {string} pathname - dynamic path
  * @param {Object} response - response object expected by the http.createServer callback
  *
  */
 server.serveDynamicContent = (request, response) => {
-  // Retrieve the HTTP method
-  const method = request.method.toLowerCase();
   // Parse the incoming request url
   const parsedUrl = url.parse(request.url, true);
   // Retrieve the pathname and query object from the parsed url
   const { pathname, query } = parsedUrl;
+  request.pathname = pathname
+  request.query = query
 
   // buffer holds the request body that might come with a POST or PUT request.
   let buffer = [];
@@ -137,12 +158,7 @@ server.serveDynamicContent = (request, response) => {
     buffer = Buffer.concat(buffer);
 
     // Prepare the request data object to pass to the handler function
-    const request = {
-      method,
-      pathname,
-      query,
-      buffer,
-    };
+    request.buffer = buffer
 
     // Retrieve the handler for the path
     const handler = server.router[pathname];
@@ -153,8 +169,13 @@ server.serveDynamicContent = (request, response) => {
      *
      */
     handler( request , (statusCode = 200, data = {}) => {
-      response.writeHead(statusCode);
-      response.end(data);
+	    if( statusCode == 301 ){
+		    response.writeHead( 301 , Object.assign( { Location: data } , corheaders ) )
+		    response.end();
+	    }else{
+		    response.writeHead( statusCode , corheaders );
+		    response.end(data);
+	    }
     });
   });
 };
@@ -163,29 +184,40 @@ server.serveDynamicContent = (request, response) => {
  *
  */
 const httpServer = http.createServer((request, response) => {
-  let pathname = url.parse(request.url, false).pathname;
-  const dynamicPath = server.getAllowedDynamicPath(pathname);
-  if (dynamicPath) {
-    server.serveDynamicContent(request, response);
-    console.log( " Dynamic " + pathname )
+
+  // Cross Domain Allowed
+  if( request.method == "OPTIONS" ){
+	console.log( "cross domain option allowed" )
+  	response.writeHead( 204 , corheaders )
+	response.end()
+	return
+  }
+  
+  let pathname = url.parse(request.url, false).pathname.replace( /\.\./g , "" )  // prevent parent directory traversal
+
+  if( server.express.hasOwnProperty( pathname ) ){  // check own property and not prototype properties HAZARD
+	console.log( " Express " + pathname )
+	request.query = url.parse( request.url , true ).query
+	//console.log( request.query )
+	server.express[ pathname ]( request , response )
+  } else if ( server.router.hasOwnProperty( pathname ) ){  // dynamic routing
+	console.log( " Dynamic " + pathname )
+	server.serveDynamicContent(request, response);
   } else {
-    pathname == "/" ? pathname = "/index.html" : true
-    server.serveStaticContent(pathname, response);
-    console.log( " Static " + pathname )
+	pathname == "/" ? pathname = "/index.html" : true  // static routing
+	console.log( " Static " + pathname )
+	server.serveStaticContent(pathname, response);
   }
 });
-
-server.router = {}
 
 /**
  * Main method to start the server
  * @param {integer} port - default value 3000
- * @param {string} host - default value 127.0.0.1
  *
  */
-server.init = (port = 3000, host = '127.0.0.1') => {
-  httpServer.listen(port, host, () => {
-    console.log(`Server is listening at http://${host}:${port}`);
+server.init = (port = 3000 ) => {
+  httpServer.listen(port, () => {
+    console.log(`Server is listening at ${port}`);
   });
 };
 
